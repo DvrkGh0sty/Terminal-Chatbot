f02a4f0593b93b4120928f7f915c39f5e1da4534
DvrkGh0sty <mutsvairoethan@gmail.com>
2025-11-25 20:17:27 +0300

    Just for fun I added a rock, paper, scissors game

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from core import Calculations
 from core import teach
 from core import get_response
+from core import game
 calc = Calculations()
 def main():
     print('Welcome to the terminal chatbot.')
@@ -13,9 +14,10 @@ def main():
             print('1. Calculate (Basic features)')
             print('2. Teach & Learn')
             print('3. Load Data')
+            print('4. Game')
 
             try:
-                option = int(input('Choose 1-3: '))
+                option = int(input('Choose 1-4: '))
             except ValueError:
                 print('Please enter a number (1 or 2).')
                 continue
@@ -62,7 +64,7 @@ def main():
                 else:
                     print('Keyword and response cannot be empty.')
 
-                        
+            # ---Load Data ---
             elif option == 3:
                 try:
                     with open("test.txt", "r") as fs:
@@ -72,16 +74,23 @@ def main():
                         print("No saved data found.")
                     else:
                         for i, line in enumerate(lines, start=1):
-                        print(f"{i}. {line.strip()}")
+                            print(f"{i}. {line.strip()}")
 
                 except FileNotFoundError:
                     print("No saved data file found yet.")
+
+                    
+            elif option == 4:
+                num_of_round = int(input("How many rounds would you like to play: "))
+                guess = input('What is your choice: ')
+                games = game(num_of_round,)
+
         elif user_input in ['/exit', '/quit']:
             print('Thank you for chat.')
             break
         else:
-            pass
-            #placeholder for actual chat
+            reply = get_response(user_input)
+            print(reply)
 
 if __name__ == '__main__':
     main()
--- core.py
+++ core.py
@@ -1,3 +1,4 @@
+import random
 class Calculations():
     def add(self, num1, num2):
         return num1 + num2
@@ -32,3 +33,65 @@ def get_response(prompt):
     else:
         print('Sorry, as we are still in beta I cannot answer your question')
 
+def game(rounds):
+    ai_score = 0
+    user_score = 0
+
+    parts = ['rock', 'paper', 'scissor']
+
+    for _ in range(rounds):
+
+        user_guess = input("Rock, Paper, or Scissor: ").strip().lower()
+        cpu_choice = random.choice(parts)
+
+        print(f"CPU chose: {cpu_choice}")
+
+        # USER LOSES
+        if user_guess == 'rock' and cpu_choice == 'paper':
+            print('CPU wins, +1 point!')
+            if user_score > 0:
+                user_score -= 1
+            ai_score += 1
+
+        elif user_guess == 'paper' and cpu_choice == 'scissor':
+            print('CPU wins, +1 point!')
+            if user_score > 0:
+                user_score -= 1
+            ai_score += 1
+
+        elif user_guess == 'scissor' and cpu_choice == 'rock':
+            print('CPU wins, +1 point!')
+            if user_score > 0:
+                user_score -= 1
+            ai_score += 1
+
+        # USER WINS
+        elif user_guess == 'paper' and cpu_choice == 'rock':
+            print('You win, +1 point')
+            if ai_score > 0:
+                ai_score -= 1
+            user_score += 1
+
+        elif user_guess == 'rock' and cpu_choice == 'scissor':
+            print('You win, +1 point')
+            if ai_score > 0:
+                ai_score -= 1
+            user_score += 1
+
+        elif user_guess == 'scissor' and cpu_choice == 'paper':
+            print('You win, +1 point')
+            if ai_score > 0:
+                ai_score -= 1
+            user_score += 1
+
+        # TIE
+        elif user_guess == cpu_choice:
+            print('TIE, no points awarded')
+
+        else:
+            print("Invalid choice. Try again.")
+            continue
+
+        print(f"Score → You: {user_score} | AI: {ai_score}\n")
+
+    return f'Final Score:\nYou: {user_score}\nAI: {ai_score}'
